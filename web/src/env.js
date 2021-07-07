c170622a3e262f0c43cd49457d5b12a370fff238
Sherjeel Afsar <sherjeelaq@gmail.com>
2021-07-07 10:49:32 +0500

    updated web

--- web/src/env.js
+++ web/src/env.js
@@ -4,18 +4,8 @@ const config = {
   // iceTransportPolicy: 'relay' // uncomment this line if you want the user to just connect with TURN server
 }
 
-// const config = {
-//   iceServers: [
-//     {
-//       urls: 'stun:stun.stunprotocol.org'
-//     }
-//   ]
-// }
+const ip = ''
 
-const ip = 'https://sfuserver.herokuapp.com'
-// const ip = 'https://invented-tundra-eagle.glitch.me'
-
-// const ip = 'http://192.168.1.33:5000'
 export default {
   config,
   ip
