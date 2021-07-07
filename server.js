6c1446c338cd4ebd68f55f579dff932c0c30b312
Sherjeel Afsar <sherjeelaq@gmail.com>
2021-07-07 10:46:21 +0500

    updated server

--- server.js
+++ server.js
@@ -12,22 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const config = {
   transportPolicy: 'relay',
-  iceServers: [
-    { urls: ['stun:ss-turn2.xirsys.com'] },
-    {
-      username:
-        'xR1Sb3-GrIK067wvB2mDWEw8MTcsOa_-1SXpds8mpz2YkCQB_zLUyGefnqWJ74b-AAAAAGB_3pBNdW5lZWJOZXNsaXQ=',
-      credential: '65c4f662-a279-11eb-89a9-0242ac140004',
-      urls: [
-        'turn:ss-turn2.xirsys.com:80?transport=udp',
-        'turn:ss-turn2.xirsys.com:3478?transport=udp',
-        'turn:ss-turn2.xirsys.com:80?transport=tcp',
-        'turn:ss-turn2.xirsys.com:3478?transport=tcp',
-        'turns:ss-turn2.xirsys.com:443?transport=tcp',
-        'turns:ss-turn2.xirsys.com:5349?transport=tcp'
-      ]
-    }
-  ]
+  iceServers: []
   // iceTransportPolicy: "relay", // uncomment this line if you want the user to just connect with TURN server
 }
 
@@ -51,8 +36,6 @@ app.post('/consumer', async ({ body }, res) => {
   const payload = {
     sdp: peer.localDescription
   }
-
-  console.log('payload is >>', payload)
   res.json(payload)
 })
 
